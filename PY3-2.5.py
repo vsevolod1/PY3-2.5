b2f2b5ca367965a306433c7eea855cf19a455044
Vsevolod <vs.kamyshev@gmail.com>
2017-03-21 13:09:30 +0300

    Переделан поиск, добавлено взаимодействие с пользователем, если результат один или ноль, програма завершается

--- PY3-2.5.py
+++ PY3-2.5.py
@@ -1,23 +1,53 @@
 import glob
 import os.path
 
-# словарь для хранения результатов
-results_dict = {}
+folder = 'Migrations'
+results = set()
+not_found = True
 
-# название папки
-migrations = 'Migrations'
+# функция вывода результатов, возвращает True, если необходимы уточнения
+def print_result (results_set):
+	if len(results_set) > 0:
+		print('Список найденных файлов:\n')
+		for item in results_set:
+			print(item)
+		print('Всего: {}\n'.format(len(results_set)))
+		if len(results_set) == 1:
+			return False
+		else:
+			return True
+	else:
+		print('Ничего не найдено\n')
+		return False
 
-# что ищем
-search = 'sp_dropuser'
 
-# открываем папку и ищес sql-файлы
-files = glob.glob(os.path.join('folder1', "*.sql"))
-for file in files:
-    with open (file) as f:
-        results_line_nums = [] #список для хранения номеров строк
-        for line_num, line in enumerate(f):
-            if search in line:
-                results_line_nums.append(line_num)
-        if len(results_line_nums) > 0:
-            results_dict[file] = results_line_nums
-print(results_dict)
+
+
+#функция для поиска
+def search (search_input, search_dict):
+	if len(search_dict) == 0:
+		files = glob.glob(os.path.join(folder, "*.sql"))
+		for file in files:
+			with open (file) as f:
+				for line in f:
+					if search_input in line:
+						search_dict.add(file)
+		return (search_dict)
+	else:
+		new_set = set()
+		for item in search_dict:
+			with open (item) as f:
+				for line in f:
+					if search_input in line:
+						new_set.add(item)
+		if len(new_set) > 0:
+			return(new_set)
+		else:
+			return({})
+
+
+# пользовательский интерфейс
+while not_found:
+    user_input = input('Введите запрос:\n')
+    results = search(user_input, results)
+    not_found = print_result(results)
